917446de698a344ba81e27fb8a6e6b9980eb3dba
Nakshatra Nambiar <nikkinambiar16@gmail.com>
2025-08-28 23:28:46 +0530

    Updated orchestrator, added new agents, removed old agents

---
+++ orchestratoragent/sub_agents/__init__.py
@@ -0,0 +1,4 @@
+from .research_agent import research_agent
+from .accessibility_agent import accessibility_agent
+from .frontend_code_agent import frontend_code_agent
+from .ui_ux_agent import ui_ux_agent
